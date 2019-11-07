dc45b23ef20f64453b78634ddc2c1eda9b22eab8
Peter Jang <peter.x.jang@gmail.com>
2019-11-06 21:59:20 -0600

    Add one product action using url segment params (example route: `localhost:3000/api/one_product_url/3`)

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
     get "/all_products_url" => "products#all_products_method"
     get "/first_product_url" => "products#first_product_method"
     get "/one_product_url" => "products#one_product_method"
+    get "/one_product_url/:id" => "products#one_product_method"
   end
 end
