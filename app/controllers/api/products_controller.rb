b95108ad4759a207e57861fdbe104ce792658015
Peter Jang <peter.x.jang@gmail.com>
2019-11-10 07:51:14 -0600

    Change one product action to follow RESTful conventions

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -4,13 +4,8 @@ class Api::ProductsController < ApplicationController
     render "index.json.jb"
   end
 
-  def first_product_method
-    @product = Product.first
-    render "first_product.json.jb"
-  end
-
-  def one_product_method
+  def show
     @product = Product.find_by(id: params["id"])
-    render "one_product.json.jb"
+    render "show.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/products" => "products#index"
-    get "/first_product_url" => "products#first_product_method"
-    get "/one_product_url" => "products#one_product_method"
-    get "/one_product_url/:id" => "products#one_product_method"
+    get "/products/:id" => "products#show"
   end
 end
