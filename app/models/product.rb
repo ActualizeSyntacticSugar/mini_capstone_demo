e6a3e46b465bf5e5d26652aec1dbd482d46af62f
Peter Jang <peter.x.jang@gmail.com>
2019-11-18 19:29:21 -0600

    Rearrange order of model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,19 +1,19 @@
 class Product < ApplicationRecord
-  belongs_to :supplier
-  has_many :images
-  # def supplier
-  #   Supplier.find_by(id: supplier_id)
-  # end
-
-  # def images
-  #   Image.where(product_id: id)
-  # end
-
   validates :name, presence: true
   validates :price, presence: true
   validates :price, numericality: { greater_than: 0 }
   validates :description, presence: true
   validates :description, length: { in: 3..1000 }
+
+  belongs_to :supplier
+  # def supplier
+  #   Supplier.find_by(id: supplier_id)
+  # end
+
+  has_many :images
+  # def images
+  #   Image.where(product_id: id)
+  # end
 
   def is_discounted?
     price < 10
